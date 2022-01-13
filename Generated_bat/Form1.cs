7fd88577c5188285a00154c851e0845d6351ca12
Сергей Выдрин <sssssssss062@gmail.com>
2022-01-13 11:00:55 +0400

    end

--- Generated_bat/Form1.cs
+++ Generated_bat/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,15 +53,46 @@ namespace Generated_bat
         }
         private void bat(string ip, string name, string password, string pc_name)
         {
-
+            Directory_data();
+            string dataname = date();
+            StreamWriter file = new StreamWriter(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\data\"+name +"_"+ dataname + ".bat", false, Encoding.UTF8);
+            //file.WriteLine(strok_csv);
+            file.WriteLine($"# прописать{ip} ip");
+            file.WriteLine($"netsh interface ip set address name=\"Ethernet\" static {ip} 255.255.255.0 10.0.77.225 1");
+            file.WriteLine($"netsh interface ip set dns \"Ethernet\" static 10.0.77.225");
+            file.WriteLine($"#создание юзера");
+            file.WriteLine($"net user {name} {password} /add");
+            file.WriteLine($"#добавление в группу админов");
+            file.WriteLine($"net localgroup Администраторы {name} /add");
+            file.WriteLine($"#имя пк и рабочей группы");
+            file.WriteLine($"wmic computersystem where name=\"%computername%\" call rename name=\"{pc_name}\"");
+            file.WriteLine($"wmic computersystem where name = \"%computername%\" call joindomainorworkgroup name=\"INSP\"");
+            file.WriteLine($"pause");
+            file.WriteLine($"shutdown /r /t 0");
+            file.Close();
         }
-
+        public string date()
+        {
+            DateTime thisDay = DateTime.Today;
+            return thisDay.ToString("dd-MM-yyyy");
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int z = 0; z < dataGridView1.RowCount; z++)
+            for (int z = 0; z <= dataGridView1.RowCount-2; z++)
             {
-                bat(dataGridView1[z, 4].Value.ToString(), dataGridView1[z, 5].Value.ToString(), dataGridView1[z, 6].Value.ToString(), dataGridView1[z, 2].Value.ToString());
-                MessageBox.Show(dataGridView1[1, z].Value.ToString());
+                //MessageBox.Show(dataGridView1[4, z].Value.ToString());
+                //MessageBox.Show(dataGridView1[4, z].Value.ToString());
+                //MessageBox.Show(dataGridView1[4, z].Value.ToString());
+                bat(dataGridView1[4,z].Value.ToString(), dataGridView1[5,z].Value.ToString(), dataGridView1[6,z].Value.ToString(), dataGridView1[7,z].Value.ToString());
+                
+            }
+        }
+        private void Directory_data()
+        {
+            if (!File.Exists(System.IO.Path.GetDirectoryName(Application.ExecutablePath) + @"\data"))
+            {
+                //log("Создаем дирректрию: " + (Application.StartupPath) + @"\data");
+                DirectoryInfo di = Directory.CreateDirectory((Application.StartupPath) + @"\data");
             }
         }
     }
